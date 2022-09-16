4d34ece0c097b1e9cbebfd528096ec30428fbfcd
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-09-16 07:48:10 +0200

    disable link, delete homepage key on package.json

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import 'react-router-dom'
 import {Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import NavBar from './components/NavBar/NavBar';
-import About from './components/About/About';
-import Tarifs from './components/Tarifs/Tarifs';
+// import About from './components/About/About';
+// import Tarifs from './components/Tarifs/Tarifs';
 // import Service from './components/NosServices/Service';
 // import Gallery from './components/Gallery/Gallery';
 
@@ -13,8 +13,8 @@ function App() {
     <NavBar/>           
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/' element={<About/>}/>        
-        <Route path='/' element={<Tarifs/>}>Tarifs</Route>        
+        {/* <Route path='/' element={<About/>}/>        
+        <Route path='/' element={<Tarifs/>}>Tarifs</Route>         */}
         {/* <Route path='/Réservation' element={<Reservation/>}>Réservation</Route> */}
         {/* <Route path='/Se connecter' element={<Login/>}>Se connecter</Route>   */}
       </Routes> 
